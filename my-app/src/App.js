8a745efad2097ff1110374f4e24ba1bd1bcd463d
Andres Davila <andres.davila-g@mail.escuelaing.edu.co>
2021-10-07 09:30:30 -0500

    react app creacion

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+
+function App() {
+  return <main>
+    <form>
+      <input type="text" placeholder="Task Name"/>
+      <button>Create task</button>
+    </form>
+    <ul>
+      <li>
+        <input checked="true" type="checkbox"/>
+        <span>Learn React</span>
+      </li>
+      <li>
+        <input type="checkbox"/>
+        <span>Learn Hooks</span>
+      </li>
+      <li>
+        <input type="checkbox"/>
+        <span>Learn Components</span>
+      </li>
+    </ul>
+  </main>
+}
+
+export default App;
