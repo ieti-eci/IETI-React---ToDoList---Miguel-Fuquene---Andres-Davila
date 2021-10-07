3e28d4fb0c466044269256c955d2f306b4b4075b
Andres Davila <andres.davila-g@mail.escuelaing.edu.co>
2021-10-07 12:40:49 -0500

    handletaskchange

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,32 @@
 import { TaskItem } from "./TaskItem";
 
 function App() {
+
+  const handleTaskChange = () => 
+  {
+    console.log("changed!");
+  }
   return <main>
     <form>
       <input type="text" placeholder="Task Name"/>
       <button>Create task</button>
     </form>
     <ul>
-      <TaskItem isChecked={true} taskName="Learn React"/>
-      <TaskItem isChecked={false} taskName="Learn Hooks"/>
-      <TaskItem isChecked={false} taskName="Keep on Keeping on"/>
+      <TaskItem 
+        isChecked={true} 
+        taskName="Learn React"
+        onTaskChange={handleTaskChange}
+      />
+      <TaskItem 
+        isChecked={false} 
+        taskName="Learn Hooks" 
+        onTaskChange={handleTaskChange} 
+      />
+      <TaskItem 
+        isChecked={false} 
+        taskName="Keep on Keeping on"
+        onTaskChange={handleTaskChange} 
+      />
     </ul>
   </main>
 }
